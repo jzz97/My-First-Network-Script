8ae93a1e71cf50926eade3d1816c8a7cfd2d9807
juanc <juanc@hotmail.com>
2024-02-16 15:25:24 -0500

    Se añadieron distintas funciones al código como tabla de enrutamiento, resolucion DNS, escaneo de puertos y demás

--- scriptdered.sh
+++ scriptdered.sh
@@ -23,3 +23,33 @@ echo "................."
 echo "3. Mostrando tabla de enrutamiento"
 nestat -rn
 echo "................."
+
+# Rsolución de DNS
+
+echo "4. Resolución DNS"
+nslookup www.google.com.co
+
+echo "...................."
+
+# Ver puertos abiertos en el host local
+
+read -p "Ingrese la dirección IP o el nombre del host para ver puertos abiertos en: " remote_host
+echo "5. Buscando puertos abiertos en $remote_host"
+nmap -p- $remote_host
+echo "......................"
+
+#Traceroute a host remoto
+read -p "Ingrese la direccion IP o el nombre del host para hacer el trace route a: " trace_host
+echo "Trazar ruta a : $trace_host"
+tracert $trace_host
+echo "....................."
+
+# Comprobar ancho de banda usando el test de velocidad por medio de la CLI de Windows
+read -p "Comprobar el ancho de banda con el test de velocidad CLI (y/n): " speedtest_option
+if ["speedtest_option" == "y"]; then
+./speedtestbyookla_x64
+
+fi 
+echo "..................."
+
+echo "Testeo de red completado"
