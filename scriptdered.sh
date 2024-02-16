89b5674ac4922a8c954c3f3582c53477eec9e3f4
juanc <juanc@hotmail.com>
2024-02-16 14:24:29 -0500

    primer commit

---
+++ scriptdered.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+# Script para probar la red con git bash en Windows
+
+echo "Network Testing Script"
+
+#Ver la conexión a Internet
+
+echo "1. Comprobando la conexión a Internet"
+
+ping -c 4 www.google.com
+
+echo "................"
